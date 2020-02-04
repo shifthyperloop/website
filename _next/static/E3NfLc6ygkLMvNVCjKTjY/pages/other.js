d4c8c978a82e23e3854281c2c12ed1216cb6123b
Hanne K. Ødegård <hanne.1998@live.no>
2020-02-05 00:24:10 +0100

    Updates

---
+++ _next/static/E3NfLc6ygkLMvNVCjKTjY/pages/other.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[26],{"+mi3":function(n,o){},AMRx:function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/other",function(){return w("+mi3")}])}},[["AMRx",0]]]);
--- common/constants.js
+++
@@ -1,1 +0,0 @@
-export const CMS_BASE_URL = 'https://cms.shifthyperloop.com';
