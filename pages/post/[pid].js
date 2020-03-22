666b3b1e87ec70d6842503165fe150b426ab0086
Hanne K. Ødegård <hanne.1998@live.no>
2020-03-22 21:47:32 +0100

    add pdf site

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -40,7 +40,7 @@ const Page = ({
             {windowSize.width > 1000 ? (
               <iframe
                 id="iframepdf"
-                src={`http://localhost:3333/?pdf=${CMS_BASE_URL + fileUrl}`}
+                src={`https://pdf-viewer.now.sh/?pdf=${CMS_BASE_URL + fileUrl}`}
               ></iframe>
             ) : (
               <a
