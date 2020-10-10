f66fe86e9b3debfeef728b37069715d4e2be8892
Ilona Podliashanyk <ilonap@stud.ntnu.no>
2020-10-10 13:56:41 +0200

    fix header
    Fixed Team ref to Team 2021 on mobile devices

--- components/Header.js
+++ components/Header.js
@@ -29,7 +29,7 @@ const Header = () => {
           Join
         </a>*/}
         <span className="menu team-menu">
-          <a href="/team">Team</a>
+          <a href="/team2021">Team</a>
           <div className="items">
             <a href="/team2021">Team 2021</a>
             <a href="/team">Team 2020</a>
