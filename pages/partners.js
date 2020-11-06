ab16fee7c13f96a60a312c5b716e89055f35d7b4
Ilona Podliashanyk <ilonap@stud.ntnu.no>
2020-11-06 13:39:05 +0100

    patch SSL abscence

--- pages/partners.js
+++ pages/partners.js
@@ -15,7 +15,7 @@ const Sponsor = ({
   return (
     <div>
       <a href={url} target="_blank" className={`sponsor ${type.toLowerCase()}`}>
-        <img src={'http://shifthyperloop01.it.ntnu.no:1337' + logoUrl} />
+        <img src={'..' + logoUrl} />
       </a>
       {type === 'Platinum' ? (
         <>
