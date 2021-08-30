0d3047c4eb6bed6fdd0d7fcc1cef6c3b0601b49e
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-08-30 17:02:24 +0200

    Add info meeting link to joinus page

--- pages/joinus.js
+++ pages/joinus.js
@@ -59,6 +59,11 @@ const Page = () => {
         contribute to the team should not hesitate to contact us any time of the
        year. */}
       </h3>
+      <div className="info-meeting-info">
+        <a className="center" href="https://fb.me/e/19LY89XOf">
+          Learn more at our info meeting September 2nd!
+        </a>
+      </div>
       <ApplyButton />
       <br />
       <div className="join-container">
@@ -105,6 +110,12 @@ const Page = () => {
 
       <style jsx>
         {`
+          .info-meeting-info {
+            margin: 10px auto;
+            text-align: center;
+            font-size: 1.2em;
+          }
+
           body {
             text-align: center;
             width: 100%;
