addc614bd2fd3de29f8360cc54bc30a81ee9bc34
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2022-01-20 19:52:18 +0100

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import '../styles.css'
+import { useEffect } from 'react';
+import ReactGA from 'react-ga4';
 
 // This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
+  useEffect(() => {
+    if(process.env.NEXT_PUBLIC_ANALYTICS_ID) {
+      ReactGA.initialize(process.env.NEXT_PUBLIC_ANALYTICS_ID);
+      ReactGA.send("pageview");
+    }
+  });
+
   return <Component {...pageProps} />
 }
+
+export default App;
