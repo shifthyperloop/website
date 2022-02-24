6b0e5e56686568dcd425790fd1912f5e834e12e0
silje.li <00silje.li@gmail.com>
2022-02-24 18:09:46 +0100

    apply to management

--- common/groupPositions.js
+++ common/groupPositions.js
@@ -1,5 +1,98 @@
 const groupPositions = [
+  
+  {
+    name: 'Structural',
+    description: '',
+    image: 'suspension.jpg',
+    positions: [
+      {
+        name: 'Group leader',
+      },
+    ],
+  },
+  {
+    name: 'Electronics & Powertrain',
+    description: '',
+    image: 'electronics.jpg',
+    positions: [
+      {
+        name: 'Group leader',
     
+      },
+    ],
+  },
+  {
+    name: 'Levitation',
+    description:
+      'The levitation group is responsible for the pod levitation and suspension systems. As a member of the levitation group you will work together with the mechanical group to stabilise the main frame, and thus the engines. Levitation is a key aspect of the hyperloop concept, and we are therefore eager to see this group exceed.',
+    image: 'levitation.jpg',
+    positions: [
+      {
+        name: 'Group leader',
+      
+      },
+    ],
+  },
+  {
+    name: 'Relations',
+    description:
+      'Shift Hyperloop is not only about building a pod, it is also about communicating with the outside world! The relations group is responsible for all things that are non-technical. This includes posting on social media, designing our website, editing videos, contacting sponsors and even organizing social events!',
+    image: 'relations.jpg',
+    positions: [
+      {
+        name: 'Group leader',
+      },
+    ],
+  },
+  {
+    name: 'Software',
+    description:
+      'The software group will create a new analysis tool and pod control interface. This will be a computer program that can be used in competition as well as during testing. The program is our way of controlling the pod, although further development would be beneficial for real time data visualization of telemetry and sensor data.',
+    image: 'software.jpeg',
+    positions: [
+      {
+        name: 'Group leader',
+      },
+    ],
+  },
+  {
+    name: 'Management',
+    description:
+      '',
+    image: 'management.jpg',
+    positions: [
+      {
+        name: 'CEO',
+         description:
+          ' ',
+      },
+      {
+        name: 'CFO',
+         description:
+          ' ',
+      },
+      {
+        name: 'CMO',
+         description:
+          'The Chief Marketing Officer is the one responsible for mantaining the image of the organisational as well as mantaining contact with the different businesspartners.',
+      },
+      {
+        name: 'CTO Mechanical',
+         description:
+          'CTO mechanical is responsible for the technical progress of the pod, with focus on the mechanical parts.',
+      },
+      {
+        name: 'CTO Electrical',
+         description:
+          'CTO electrical is responsible for the technical progress of the pod, with focus on the electrical solutions. ',
+      },
+      {
+        name: 'CCO',
+         description:
+          ' ',
+      },
+    ]
+  },
 ];
 
 export default groupPositions;
