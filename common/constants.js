fed735e757e751fca7076c28b9551e864930e1da
Ole-Jakob SHT <ole.schubert@shifthyperloop.com>
2022-10-02 15:46:37 +0200

    Fjernet kommentar under Apply

--- common/constants.js
+++ common/constants.js
@@ -1,4 +1,4 @@
 export const CMS_BASE_URL = 'https://shifthyperloop01.it.ntnu.no/strapi';
 export const APPLY_URL =
   'https://docs.google.com/forms/d/e/1FAIpQLSdP4rW0ypXGMCc8LVk-tV4-0A9unLXi0LvTg9Jn4H4mQapp9g/viewform?usp=sf_link';
-export const APPLY_DEADLINE = 'Dere skal være velkomne!';
+export const APPLY_DEADLINE = undefined;
